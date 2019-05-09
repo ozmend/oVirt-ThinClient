7995768c17c17fac72dd6a9837999a6dc7cec614
ozmend <ozmen62@hotmail.com>
2019-05-09 08:50:19 +0300

    Add files via upload

---
+++ conf/langs/en_US.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>LoginDialog</name>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="53"/>
+        <source>Remote Desktop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="61"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="62"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="63"/>
+        <source>Domain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="79"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="81"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="129"/>
+        <source>Unsuccessful!!!
+Username/Password incorrect or account is locked.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="131"/>
+        <source>Username or Password cannot be empty!!!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ManageDialog</name>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="151"/>
+        <source>oVirt VDI Client Connections</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="158"/>
+        <source>My Connections</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="175"/>
+        <source>Run</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="176"/>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="177"/>
+        <source>PowerOff</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="180"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="206"/>
+        <source>Monitor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="283"/>
+        <source>ATTENTION!!!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../oVirt_Client-beta.py" line="283"/>
+        <source>This process can cause undesirable data lost.
+
+        Do you want to proceed?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
